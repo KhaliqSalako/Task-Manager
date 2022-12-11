ae114dfef0bde99afc05f7b3f0ece0d6e2dfd76b
Khaliq <itskhaliq@icloud.com>
2022-12-11 00:35:39 -0600

    Feature 10 complete

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from projects.models import Project
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def project_list(request):
+    projects = Project.objects.filter(owner=request.user)
+    context = {
+        "projects": projects,
+    }
+    return render(request, "projects/list.html", context)
